591bec99b198ae086dc258142e5ff8983368beeb
Mihai Sarto <sarto.mihai@gmail.com>
2013-11-22 11:47:05 -0800

    Optimize logger

--- src/org/avario/engine/sensors/BaroSensorThread.java
+++ src/org/avario/engine/sensors/BaroSensorThread.java
@@ -4,6 +4,7 @@ import org.avario.engine.DataAccessObject;
 import org.avario.engine.SensorProducer;
 import org.avario.engine.SensorThread;
 import org.avario.engine.prefs.Preferences;
+import org.avario.utils.Logger;
 import org.avario.utils.filters.sensors.BaroSensorFilter;
 
 import android.app.Activity;
@@ -33,7 +34,7 @@ public class BaroSensorThread extends SensorThread<Float> {
 		final float diff = Math.abs(prevPresure - currentPresure);
 		if (diff > ((50 - Preferences.baro_sensitivity) * 0.002f)) {
 			// We will skip big consecutive differences to filter the big noise
-			// Logger.get().log("Skip " + diff);
+			Logger.get().log("Skip " + diff);
 			int filterSensitivity = (100 - Preferences.baro_sensitivity);
 			currentPresure = prevPresure > 0 ? ((prevPresure * filterSensitivity + currentPresure
 					* (100 - filterSensitivity)) / 100) : currentPresure;
--- src/org/avario/utils/Logger.java
+++ src/org/avario/utils/Logger.java
@@ -12,7 +12,7 @@ import android.os.Environment;
 import android.util.Log;
 
 public class Logger {
-	private static boolean USE_LOG = false;
+	public static boolean USE_LOG = false;
 	private static final String LOG_TAG = "AVARIO";
 	private static Logger THIS = new Logger();
 	private OutputStream logStream = null;
