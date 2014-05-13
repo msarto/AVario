b0692daf7cf13039199472586772ea6b621ee6b9
Mihai Sarto <sarto.mihai@gmail.com>
2014-05-13 16:22:31 +0300

    Update IGC with G signature

---
+++ src/org/avario/engine/sounds/tone/LiftTone.java
@@ -0,0 +1,31 @@
+package org.avario.engine.sounds.tone;
+
+import org.avario.engine.prefs.Preferences;
+import org.avario.engine.sounds.AsyncTone;
+import org.avario.utils.Logger;
+
+public class LiftTone extends AsyncTone {
+	protected TonePlayer tonePlayer = new TonePlayer();
+	protected float beepHz;
+	
+	public void setSpeed(float beepSpeed) {
+		 beepHz = Preferences.lift_hz + Preferences.tone_variation * beepSpeed;
+		super.setSpeed(beepSpeed);
+	}
+	
+	@Override
+	public void beep() {
+		if (!isPlaying) {
+			try {
+				isPlaying = true;
+				tonePlayer.play(beepHz, TonePlayer.ToneType.HIGH);
+				Thread.sleep(Math.round(Preferences.beep_interval - (Preferences.beep_interval / 6) * beepSpeed));
+				tonePlayer.stop();
+			} catch (InterruptedException e) {
+				Logger.get().log("Fail lift beep ", e);
+			} finally {
+				isPlaying = false;
+			}
+		}
+	}
+}
